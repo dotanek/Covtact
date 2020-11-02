bd27d9698ccfe1ea1e7178f947b0ce763357b6e6
unknown <dotanek222@gmail.com>
2020-11-02 16:26:25 +0100

    Basic app structure.

--- app/src/main/java/com/bigpharma/covtact/MainActivity.java
+++ app/src/main/java/com/bigpharma/covtact/MainActivity.java
@@ -2,13 +2,63 @@ package com.bigpharma.covtact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Buttons
+
+    private Button contactsBtn;
+    private Button settingsBtn;
+    private Button mapBtn;
+    private Button reportBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButtons();
+    }
+
+    private void initButtons() {
+        contactsBtn = (Button) findViewById(R.id.contactsBtn);
+        contactsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent contactsIntent = new Intent(view.getContext(),ContactsActivity.class);
+                view.getContext().startActivity(contactsIntent);
+            }
+        });
+
+        settingsBtn = (Button) findViewById(R.id.settingsBtn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settingsIntent = new Intent(view.getContext(),SettingsActivity.class);
+                view.getContext().startActivity(settingsIntent);
+            }
+        });
+
+        mapBtn = (Button) findViewById(R.id.mapBtn);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapIntent = new Intent(view.getContext(),MapActivity.class);
+                view.getContext().startActivity(mapIntent);
+            }
+        });
+
+        reportBtn = (Button) findViewById(R.id.reportBtn);
+        reportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reportIntent = new Intent(view.getContext(),ReportActivity.class);
+                view.getContext().startActivity(reportIntent);
+            }
+        });
     }
 }
