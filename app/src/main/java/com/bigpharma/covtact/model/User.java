7bdc651e5a4111b9c64e6f7ac3ce79256299b5f6
224350 Kulesza Wojciech <224350@edu.p.lodz.pl>
2021-01-14 16:27:27 +0100

    login-final

---
+++ app/src/main/java/com/bigpharma/covtact/model/User.java
@@ -0,0 +1,12 @@
+package com.bigpharma.covtact.model;
+
+public class User {
+    public String name, email;
+    public User(){
+
+    }
+    public User(String name, String email){
+        this.name=name;
+        this.email=email;
+    }
+}
